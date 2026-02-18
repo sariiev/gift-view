3d96f029394a6d511190245577fa6b511706431a
lanoitarri <veiirasyhyh@gmail.com>
2026-02-18 17:21:44 +0100

    feat(db): add a function for fetching all assets

--- src/gift_view/db/repositories/asset_repository.py
+++ src/gift_view/db/repositories/asset_repository.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,6 +9,14 @@ from gift_view.db.models import Asset
 class AssetRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+
+    async def get_all(self) -> List[Asset]:
+        res = await self.session.execute(
+            select(Asset)
+        )
+
+        return list(res.scalars().all())
 
 
     async def get_by_symbol(self, symbol: str) -> Optional[Asset]:
