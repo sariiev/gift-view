93988be9a8dd5db23c5bc3bd4d570ddd9e2efc3a
lanoitarri <veiirasyhyh@gmail.com>
2026-02-20 22:31:48 +0100

    refactor(db): add unique constraint on name and rarity

--- src/gift_view/db/models/backdrop.py
+++ src/gift_view/db/models/backdrop.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Float
+from sqlalchemy import String, Float, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from gift_view.db.base import Base
@@ -8,7 +8,7 @@ class Backdrop(Base):
     __tablename__ = "backdrops"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True)
+    name: Mapped[str] = mapped_column(String(50))
     rarity_percent: Mapped[float] = mapped_column(Float)
 
     sales = relationship("Sale", back_populates="backdrop")
@@ -16,4 +16,8 @@ class Backdrop(Base):
         "Gift",
         secondary="gifts_backdrops",
         back_populates="backdrops",
+    )
+
+    __table_args__ = (
+        UniqueConstraint("name", "rarity_percent"),
     )
--- src/gift_view/db/models/symbol.py
+++ src/gift_view/db/models/symbol.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Float
+from sqlalchemy import String, Float, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from gift_view.db.base import Base
@@ -8,7 +8,7 @@ class Symbol(Base):
     __tablename__ = "symbols"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True)
+    name: Mapped[str] = mapped_column(String(50))
     rarity_percent: Mapped[float] = mapped_column(Float)
 
     sales = relationship("Sale", back_populates="symbol")
@@ -16,4 +16,8 @@ class Symbol(Base):
         "Gift",
         secondary="gifts_symbols",
         back_populates="symbols",
+    )
+
+    __table_args__ = (
+        UniqueConstraint("name", "rarity_percent"),
     )
