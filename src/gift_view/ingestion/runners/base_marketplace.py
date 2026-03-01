5897da74b64330b72afbcd6b6702eb7fc1e9bd09
lanoitarri <veiirasyhyh@gmail.com>
2026-03-01 16:53:49 +0100

    feat(ingestion): introduce base marketplace runner

---
+++ src/gift_view/ingestion/runners/base_marketplace.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import AsyncContextManager, Callable
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from gift_view.ingestion.clients import BaseMarketplaceClient
+from gift_view.ingestion.parsers import BaseMarketplaceParser
+
+
+class BaseMarketplaceRunner(ABC):
+    def __init__(
+            self,
+            marketplace_client: BaseMarketplaceClient,
+            marketplace_parser: BaseMarketplaceParser,
+            session_factory: Callable[[], AsyncContextManager[AsyncSession]]
+    ):
+        self.marketplace_client = marketplace_client
+        self.marketplace_parser = marketplace_parser
+        self.session_factory = session_factory
+
+
+    @abstractmethod
+    async def run_once(self):
+        pass
