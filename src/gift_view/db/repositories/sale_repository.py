5f736b0eb0811050214db7210df7ac098921ad31
lanoitarri <veiirasyhyh@gmail.com>
2026-03-01 16:52:49 +0100

    refactor(db): use ON CONFLICT DO NOTHING in SaleRepository

--- src/gift_view/db/repositories/sale_repository.py
+++ src/gift_view/db/repositories/sale_repository.py
@@ -1,6 +1,8 @@
 from typing import List
 
+from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.util import await_only
 
 from gift_view.db.models import Sale
 
@@ -10,9 +12,29 @@ class SaleRepository:
         self.session = session
 
 
-    def add(self, sale: Sale):
-        self.session.add(sale)
+    async def add_all(self, sales: List[Sale]):
+        values = [
+            {
+                "marketplace_id": sale.marketplace_id,
+                "marketplace_sale_id": sale.marketplace_sale_id,
+                "gift_id": sale.gift_id,
+                "gift_number": sale.gift_number,
+                "model_id": sale.model_id,
+                "backdrop_id": sale.backdrop_id,
+                "symbol_id": sale.symbol_id,
+                "asset_id": sale.asset_id,
+                "price_native": sale.price_native,
+                "price_usd": sale.price_usd,
+                "timestamp": sale.timestamp
+            } for sale in sales
+        ]
 
+        stmt = (
+            insert(Sale)
+            .values(values)
+            .on_conflict_do_nothing(
+                index_elements=["marketplace_id", "marketplace_sale_id"]
+            )
+        )
 
-    def add_all(self, sales: List[Sale]):
-        self.session.add_all(sales)
+        await self.session.execute(stmt)
