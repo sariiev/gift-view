08ed95dec9326ae500dc3f9d8d381acf63aa4f37
lanoitarri <veiirasyhyh@gmail.com>
2026-03-09 13:58:41 +0100

    refactor(aggregation): make intervals parameter optional; use interval_delta to rewrite latest bar

--- src/gift_view/aggregation/aggregators/gift_price_bar.py
+++ src/gift_view/aggregation/aggregators/gift_price_bar.py
@@ -1,6 +1,6 @@
 from datetime import timedelta, datetime, timezone
 from logging import getLogger
-from typing import Callable, AsyncContextManager, List
+from typing import Callable, AsyncContextManager, List, Optional
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -11,7 +11,7 @@ class GiftPriceBarAggregator:
     def __init__(
             self,
             session_factory: Callable[[], AsyncContextManager[AsyncSession]],
-            intervals: List[str]
+            intervals: Optional[List[str]] = None
     ):
         self.session_factory = session_factory
         self.intervals = intervals or ["1h", "4h", "1d", "1w"]
@@ -23,16 +23,16 @@ class GiftPriceBarAggregator:
         async with self.session_factory() as session:
             gift_price_bar_repository = GiftPriceBarRepository(session=session)
             for interval in self.intervals:
-                self.logger.info("Aggregating gift sales for %s interval", interval)
                 start = await gift_price_bar_repository.get_latest_timestamp(interval=interval)
 
                 if start is None:
                     start = datetime(2025, 1, 1, tzinfo=timezone.utc)
                 else:
-                    start -= timedelta(hours=1)
+                    start -= self.interval_delta(interval=interval)
 
+                self.logger.info("Aggregating gift sales for %s interval from %s", interval, start)
                 await gift_price_bar_repository.aggregate(start=start, interval=interval)
-                self.logger.info("Aggregated gift sales for %s interval", interval)
+                self.logger.info("Aggregated gift sales for %s interval (start=%s)", interval, start)
 
 
     def interval_delta(self, interval: str) -> timedelta:
